a5ed4b6beb24674b967087f533c4e4f1a8a4ad32
ricecake88 <vocalise@gmail.com>
2019-06-29 15:33:39 -0700

    Added Devise files - preliminary

--- db/migrate/20190627033539_create_users.rb
+++ db/migrate/20190627033539_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :place_id
+      t.integer :place_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_052449) do
+ActiveRecord::Schema.define(version: 2019_06_29_205112) do
 
   create_table "attractions", force: :cascade do |t|
     t.integer "place_id"
@@ -58,9 +58,16 @@ ActiveRecord::Schema.define(version: 2019_06_27_052449) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "place_id"
+    t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "vacation_places", force: :cascade do |t|
