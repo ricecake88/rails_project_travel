7c233c5b49797c4311612a6ee39dd01c175720fc
learn <vocalise@gmail.com>
2019-06-29 20:45:31 -0700

    Added additional routes and basic views in vacation controller

--- app/controllers/vacations_controller.rb
+++ app/controllers/vacations_controller.rb
@@ -1,5 +1,28 @@
 class VacationsController < ApplicationController
 
     def index
+        @vacations = Vacation.all
+    end
+
+    def new
+        @vacation = Vacation.new
+    end
+
+    def show
+    end
+
+    def create
+        @vacation = Vacation.create!(vacation_params)
+    end
+
+    def update
+        @vacation = Vacation.find_by(:id => params[:id])
+    end
+
+    def destroy
+    end
+
+    private
+    def vacation_params
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'vacations#index'
-  resources :vacations
+  resources :vacations, only: [:index, :show] do 
+    resources :legs, only: [:index, :show]
+  end
+
+  resources :vacations, only: [:create, :update, :destroy]
 end
