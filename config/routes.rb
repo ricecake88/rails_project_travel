1820f16856058e9c31d8273651b4a2f203ceb426
ricecake88 <vocalise@gmail.com>
2019-07-07 10:37:17 -0700

    Removed nested routes for now to get preliminary routes working

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'vacations#index'
-  resources :vacations, only: [:index, :show] do 
-    resources :legs, only: [:index, :show]
-  end
 
-  resources :vacations, only: [:create, :update, :destroy]
+  resources :vacations
 
   devise_for :users, :controllers => { 
     omniauth_callbacks: 'users/omniauth_callbacks',
