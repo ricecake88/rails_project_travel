b826d4dc83f8867797f5eb7c7ee8acdf92640d62
ricecake88 <vocalise@gmail.com>
2019-07-07 13:04:06 -0700

    Updated create, edit, destroy, update and all vacation_params

--- app/controllers/vacations_controller.rb
+++ app/controllers/vacations_controller.rb
@@ -9,20 +9,40 @@ class VacationsController < ApplicationController
     end
 
     def show
+        @vacation = Vacation.find_by(:id => params[:id])
+    end
+
+    def edit
+        @vacation = Vacation.find_by(:id => params[:id])
     end
 
     def create
-        @vacation = Vacation.create!(vacation_params)
+       @vacation = Vacation.new(vacation_params)
+       @vacation.user = current_user
+       if @vacation.save
+        flash[:notice] = "Vacation Created."
+        redirect_to vacation_path(@vacation)
+       else
+        flash[:notice] = "Test"
+        redirect_to root_path
+       end
     end
 
     def update
         @vacation = Vacation.find_by(:id => params[:id])
+        @vacation.update(vacation_params)
+        redirect_to vacation_path(@vacation)
     end
 
     def destroy
+        @vacation = Vacation.find_by(params[:id])
+        @vacation.destroy
+        flash[:notice] = "Vacation Deleted."
+        redirect_to vacations_path
     end
 
     private
     def vacation_params
+        params.require(:vacation).permit(:name, :start_date, :end_date)
     end
 end
