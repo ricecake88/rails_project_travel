48ad231dbe272d014a1250eab97d06abc6c7da69
learn <vocalise@gmail.com>
2019-07-13 21:47:14 -0700

    Changed contests of legs migration

--- db/migrate/20190627034034_create_legs.rb
+++ db/migrate/20190627034034_create_legs.rb
@@ -1,12 +1,19 @@
 class CreateLegs < ActiveRecord::Migration[5.2]
   def change
     create_table :legs do |t|
-      t.string :country_name
-      t.string :country_code
-      t.string :state_name
-      t.string :state_code
-      t.string :city_name
+      t.string :arrival_country_name
+      t.string :arrival_country_code
+      t.string :arrival_state_name
+      t.string :arrival_state_code
+      t.string :arrival_city_name
       t.integer :vacation_id
+
+      t.string :departure_country_name
+      t.string :departure_country_code
+      t.string :departure_state_name
+      t.string :departure_state_code
+
+      t.string :departure_city_name
 
       t.timestamps
     end
