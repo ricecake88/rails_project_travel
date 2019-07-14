422c8f66b32892d55663f652fe3fcd4b924d7eea
ricecake88 <vocalise@gmail.com>
2019-07-14 13:44:55 -0700

    Removed vacation id calls for item itinerary creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,13 +23,13 @@
 @user1_vacation2 = Vacation.create(:user_id => 1, :name => "Seattle With Family")
 @user1_vacation2_leg1 = Leg.create(:vacation_id => @user1_vacation2.id, :arrival_city_name => @seattle.id, :departure_city_name => @sandiego.id)
 
-@user_v1_l1_item1 = ItineraryItem.create(:leg_id => @user1_vacation1_leg1.id, :vacation_id => @user1_vacation1.id, :day_of_trip => 1,
+@user_v1_l1_item1 = ItineraryItem.create(:leg_id => @user1_vacation1_leg1.id, :day_of_trip => 1,
     :type_of_activity => "Activity", :attraction_id => @seattle_1.id, :activity_order => 1)
-@user_v1_l1_item2 = ItineraryItem.create(:leg_id => @user1_vacation1_leg1.id, :vacation_id => @user1_vacation1.id, :day_of_trip => 2,
+@user_v1_l1_item2 = ItineraryItem.create(:leg_id => @user1_vacation1_leg1.id, :day_of_trip => 2,
     :type_of_activity => "Activity", :attraction_id => @seattle_2.id, :activity_order => 1)
-@user_v1_l2_item1 = ItineraryItem.create(:leg_id => @user1_vacation1_leg2.id, :vacation_id => @user1_vacation1.id, :day_of_trip => 3,
+@user_v1_l2_item1 = ItineraryItem.create(:leg_id => @user1_vacation1_leg2.id, :day_of_trip => 3,
     :type_of_activity => "Activity", :attraction_id => @banff_1.id, :activity_order => 1)
 
-@user_v2_l1_item1 = ItineraryItem.create(:leg_id => @user1_vacation2_leg1.id, :vacation_id => @user1_vacation2.id, :day_of_trip => 1,
+@user_v2_l1_item1 = ItineraryItem.create(:leg_id => @user1_vacation2_leg1.id, :day_of_trip => 1,
     :type_of_activity => "Transport", :mode_of_transport => "Airplane", :activity_order=> 1)
 
