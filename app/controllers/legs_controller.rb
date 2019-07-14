a7c532cd9506db47479af084e3e26c652314c425
learn <vocalise@gmail.com>
2019-07-13 21:46:45 -0700

    Preliminary controller actions added to legs controller

--- app/controllers/legs_controller.rb
+++ app/controllers/legs_controller.rb
@@ -1,9 +1,37 @@
 class LegsController < ApplicationController
     def index
-        @legs = current_user.legs
+        if (params[:vacation_id])
+            @legs = Vacation.find_by(:id => params[:vacation_id]).legs
+        else
+            flash[:notice] = "Cannot show this page"
+        end
+    end
+
+    def new
+        @leg = Leg.new(:vacation_id => params[:vacation_id])
     end
 
     def show
         @leg = Leg.find_by(:id => params[:id])
+        binding.pry
     end
+
+    def create
+        if params[:vacation_id]
+        else
+        end
+        @leg = Leg.new(leg_params)
+        if @leg.save!
+            redirect_to vacation_leg_path(@leg.vacation)
+        else
+            flash[:notice] = "Fail"
+            render new
+        end
+    end
+
+    private
+    def leg_params
+        params.require(:leg).permit(:arrival_city_name, :vacation_id)
+    end
+
 end
