7e2e369137ff28a0710f4fae1882823cc9d147c4
learn <vocalise@gmail.com>
2019-08-31 19:06:55 -0700

    Added validation statements for presence of city name and country name

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,7 @@ class Place < ApplicationRecord
     has_many :vacation_places    
     has_many :vacations, through: :vacation_places
     has_many :attractions
+    validates :city_name, presence: { message: 'City cannot be blank.' }
+    validates :country_name, presence: { message: 'Country cannot be blank.' }
+    validates_uniqueness_of :city_name, scope: [:state_name, :country_name]
 end
