f4e2556543d6e25b7e1e922f5e28233c9342eeb8
learn <vocalise@gmail.com>
2019-09-01 20:34:26 -0700

    Added comment on to do action for place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,5 +4,6 @@ class Place < ApplicationRecord
     has_many :attractions
     validates :city_name, presence: { message: 'City cannot be blank.' }
     validates :country_name, presence: { message: 'Country cannot be blank.' }
-    validates_uniqueness_of :city_name, scope: [:state_name, :country_name]
+    validates_uniqueness_of :city_name, scope: [:state_name, :country_nameh]
+    #todo: check no strange characters
 end
