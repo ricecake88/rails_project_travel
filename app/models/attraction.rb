5ca03de7c1e0b254caf4e0c7a2249fdfecfcf090
ricecake88 <vocalise@gmail.com>
2019-09-07 16:37:37 -0700

    Updated Attraction model with function that only retrieves attractions within a place

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -2,4 +2,8 @@ class Attraction < ApplicationRecord
     belongs_to :place
     validates :name, presence: {message: 'Name cannot be blank.'}
     validates_uniqueness_of :name, scope: [:place_id], :message => 'Already entered.'
+
+    def self.locations_in_place(leg)
+        Attraction.where(:place_id => Place.find_by(:city_name => leg.arrival_city_name).id)
+    end
 end
