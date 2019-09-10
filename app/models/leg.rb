67835458dbde63567e49b92ac6fc2e967df38fae
ricecake88 <vocalise@gmail.com>
2019-09-09 22:32:42 -0700

    Add a method to insert places to vacations.places array as well as place information to leg

--- app/models/leg.rb
+++ app/models/leg.rb
@@ -2,16 +2,17 @@ class Leg < ApplicationRecord
     belongs_to :vacation
     has_many :itinerary_items
 
-    def self.insert_places(params)
-        vacation = Vacation.find_by(:id => params[:leg][:vacation_id])
-        arrival_place = Place.find_by(:id => params[:leg][:arrival_place_id])
-        departure_place = Place.find_by(:id => params[:leg][:departure_place_id])
-        if vacation.places.include?(arrival_place)
+    def self.insert_and_update_places(leg)
+        vacation = Vacation.find_by(:id => leg.vacation_id)
+        arrival_place = Place.find_by(:id => leg.arrival_place_id)
+        departure_place = Place.find_by(:id => leg.departure_place_id)
+        if arrival_place.present? && vacation.places.include?(arrival_place)
             vacation.places << arrival_place
+            leg.update(:arrival_city_name => arrival_place.city_name)
         end
-        if vacation.places.include?(departure_place)
+        if departure_place.present? && vacation.places.include?(departure_place)
             vacation.places << departure_place
+            leg.update(:departure_city_name => departure_place.city_name)
         end
-
     end
 end
