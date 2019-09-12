a03aa13e1afb0dac35cefd614c07fd9a2bafc1ab
ricecake88 <vocalise@gmail.com>
2019-09-11 22:24:46 -0700

    Added helper method to retrieve attraction name using attraction id

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def self.place(place_id)
+        Place.find_by(:id => place_id)
+    end
+
+    def self.attraction(object)
+        Attraction.find_by(:id => object.attraction_id)
+    end
 end
