08e15a78f7b941519a951ac4545d5f3c89eed386
ricecake88 <vocalise@gmail.com>
2019-10-10 22:53:11 -0700

    Update validations for Itinerary Item model

--- app/models/itinerary_item.rb
+++ app/models/itinerary_item.rb
@@ -1,5 +1,11 @@
 class ItineraryItem < ApplicationRecord
     belongs_to :leg
+    validates :attraction_id, presence: true, if: :event_type?
+    validates :day_of_trip, presence: true
+
+    def event_type?
+        type_of_activity == "Event"
+    end
 
     def self.sort(leg_id)
         ItineraryItem.where(:leg_id => leg_id).order(:day_of_trip)
