34c33fb3336ac8773c4f1d794ee614857ec8f0b0
ricecake88 <vocalise@gmail.com>
2019-10-10 22:52:36 -0700

    Updated Itinerary Item controller to check for authorization, and updated Vacation model with method to retrieve authorization

--- app/controllers/itinerary_items_controller.rb
+++ app/controllers/itinerary_items_controller.rb
@@ -1,18 +1,24 @@
 class ItineraryItemsController < ApplicationController
     def index
         if params[:leg_id]
-            @itinerary_items = ItineraryItem.sort(params[:leg_id])
+            if (Vacation.permit_view_by_leg_id(current_user, params[:leg_id])).present?
+                @itinerary_items = ItineraryItem.sort(params[:leg_id])
+            else
+                redirect_to vacations_path, alert: "You don't have permission to do that"
+            end
         else
-            flash[:notice] = "Not used."
+            flash[:notice] = "Not Used."
         end
     end
 
     def new
-        if params[:leg_id] && !Leg.exists?(params[:leg_id])
-            redirect_to legs_path, alert: "Leg Not Found."
-        else
-            @itinerary_item = ItineraryItem.new(:leg_id => params[:leg_id])
-            @attractions = Attraction.locations_in_place(@itinerary_item.leg)
+        if params[:leg_id]
+            if (Vacation.permit_view_by_leg_id(current_user, params[:leg_id])).present?
+                @itinerary_item = ItineraryItem.new(:leg_id => params[:leg_id])
+                @attractions = Attraction.locations_in_place(@itinerary_item.leg)
+            else
+                redirect_to vacations_path, alert: "You don't have permission to do that."
+            end
         end
     end
 
@@ -36,38 +42,54 @@ class ItineraryItemsController < ApplicationController
 
     def create
         if params[:itinerary_item][:leg_id]
-            @itinerary_item = ItineraryItem.new(itinerary_item_params)
-            @leg = @itinerary_item.leg
-            if @itinerary_item.save!
-                redirect_to leg_itinerary_item_path(@leg, @itinerary_item)
+            if (Vacation.permit_view_by_leg_id(current_user, params[:itinerary_item][:leg_id])).present?
+                @itinerary_item = ItineraryItem.new(itinerary_item_params)
+                if @itinerary_item.save
+                    redirect_to leg_itinerary_item_path(@itinerary_item.leg, @itinerary_item)
+                else
+                    flash[:notice] = flash_error_message(@itinerary_item)
+                    redirect_to new_leg_itinerary_item_path(@itinerary_item.leg)
+                end
             else
-                redirect_to new_leg_itinerary_item_path(@leg), alert: "Could not Create Item"
+                redirect_to vacations_path, alert: "You don't have permission to do that."
             end
         end
     end
 
     def update
         if params[:itinerary_item][:leg_id]
-            @itinerary_item = ItineraryItem.find_by(:id => params[:id])
-            if @itinerary_item.update(itinerary_item_params)
-                redirect_to leg_itinerary_item_path(@itinerary_item.leg, @itinerary_item)
+            if (Vacation.permit_view_by_leg_id(current_user, params[:itinerary_item][:leg_id])).present?
+                @itinerary_item = ItineraryItem.find_by(:id => params[:id])
+                if @itinerary_item.update(itinerary_item_params)
+                    redirect_to leg_itinerary_item_path(@itinerary_item.leg, @itinerary_item)
+                else
+                    redirect_to edit_leg_itinerary_item_path(@leg), alert: "Could Not Edit Item."
+                end
             else
-                redirect_to edit_leg_itinerary_item_path(@leg), alert: "Could Not Edit Item."
+                redirect_to vacations_path, alert: "You don't have permission to do that."
             end
         end
     end
 
     def destroy
         @item = ItineraryItem.find_by(:id => params[:id])
-        @leg = @item.leg
-        @item.destroy
-        flash[:notice] = "Item deleted."
-        redirect_to leg_path(@leg)
+        if (Vacation.permit_view_by_leg_id(current_user, @item.leg.id)).present?
+            @leg = @item.leg
+            @item.destroy
+            flash[:notice] = "Item deleted."
+            redirect_to leg_path(@leg)
+        else
+            redirect_to vacations_path, alert: "You don't have permission to do that."
+        end
     end
 
     private
     def itinerary_item_params
         params.require(:itinerary_item).permit(:leg_id, :time, :day_of_trip, :type_of_activity, :attraction_id)
+    end
+
+    def flash_error_message(arg)
+        "The form contains #{ActionController::Base.helpers.pluralize(arg.errors.count, 'error')}: #{arg.errors.full_messages.join(', ')}".html_safe
     end
 
 end
--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -7,6 +7,19 @@ class Vacation < ApplicationRecord
     validates :name, presence: { message: "Name cannot be blank." }
 
     def self.permit_view(user, object)
-        Vacation.where("user_id = ? and id = ?", user.id, object.vacation.id)
+        if object.present?
+            @vacation = Vacation.where("user_id = ? and id = ?", user.id, object.vacation.id)
+        else
+            nil
+        end
+    end
+
+    def self.permit_view_by_leg_id(user, leg_id)
+        leg = Leg.find_by(:id => leg_id)
+        if leg.present?
+            @vacation = Vacation.where("user_id = ? and id = ?", user.id, leg.vacation.id)
+        else
+            nil
+        end
     end
 end
