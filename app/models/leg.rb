053aaae9c5b7c08e7dbec562f8bed18b1881b067
ricecake88 <vocalise@gmail.com>
2019-10-16 21:17:01 -0700

    Added class  method for Leg that would sort the legs in order of leg num

--- app/models/leg.rb
+++ app/models/leg.rb
@@ -1,4 +1,8 @@
 class Leg < ApplicationRecord
     belongs_to :vacation
     has_many :itinerary_items
+
+    def self.sort(vacation_id)
+        Leg.where(:vacation_id => vacation_id).order(:leg_num)
+    end
 end
