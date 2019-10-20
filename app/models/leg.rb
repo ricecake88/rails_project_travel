6ebe93312273406f1f535e96bf3ed21bc9e79d07
ricecake88 <vocalise@gmail.com>
2019-10-20 12:51:07 -0700

    Added validation for leg model

--- app/models/leg.rb
+++ app/models/leg.rb
@@ -1,8 +1,17 @@
 class Leg < ApplicationRecord
     belongs_to :vacation
     has_many :itinerary_items
+    validates :departure_place_id, presence: true
+    validates :arrival_place_id, presence: true
+    validate :arrival_departure_cannot_be_same
 
     def self.sort(vacation_id)
         Leg.where(:vacation_id => vacation_id).order(:leg_num)
+    end
+
+    def arrival_departure_cannot_be_same
+        if arrival_place_id == departure_place_id
+            errors.add(:departure_place_id, "cannot be the same as arrival location")
+        end
     end
 end
