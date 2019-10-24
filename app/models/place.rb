35986554f5f1ece10b8ce08ae29642611435ddcc
ricecake88 <vocalise@gmail.com>
2019-10-23 22:03:14 -0700

    Added validation for city names and country names

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,10 +2,10 @@ class Place < ApplicationRecord
     has_many :vacation_places
     has_many :vacations, through: :vacation_places
     has_many :attractions
-    validates :city_name, presence: { message: 'City cannot be blank.' }
-    validates :country_name, presence: { message: 'Country cannot be blank.' }
-    validates_uniqueness_of :city_name, scope: [:state_name, :country_name]
-    #todo: check no strange characters
+    validates :city_name, presence: { message: 'cannot be blank.' }
+    validates :country_name, presence: { message: 'cannot be blank.' }
+    validates_uniqueness_of :city_name, scope: [:state_name, :country_name], message: 'already entered.'
+    validates_format_of :city_name, :country_name, :with => /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/
 
     def self.user_places(user)
         #@vacations = VacationPlace.where(vacation_id: user.vacations)
@@ -20,4 +20,5 @@ class Place < ApplicationRecord
         end
         return user_places
     end
+
 end
