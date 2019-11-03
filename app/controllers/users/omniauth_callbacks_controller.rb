b4f8125e000a5ba83cb0223893e32488d019210c
ricecake88 <vocalise@gmail.com>
2019-11-03 15:00:00 -0800

    Cleaned up def google_oauth2

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,12 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
         @user = User.from_omniauth(request.env['omniauth.auth'])
         puts @user.errors.to_a
-        #if @user
-        #    sign_in @user
-        #    redirect_to root_path
-        #else
-        #    redirect_to new_user_session_path, notice: "Access Denied."
-        #end
+
         if @user.persisted?
             flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
             sign_in_and_redirect @user, :event => :authentication
