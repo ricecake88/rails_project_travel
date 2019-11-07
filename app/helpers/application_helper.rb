65acd5e4b9c80ed085c2cac44c6bb02b0890637b
ricecake88 <vocalise@gmail.com>
2019-11-06 21:57:51 -0800

    Added nav_link method to set list to active when user is on associated page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
     def flash_error_message(arg)
         "The form contains #{ActionController::Base.helpers.pluralize(arg.errors.count, 'error')}:<br> #{arg.errors.full_messages.join('<br> ')}"
     end
+
+    def nav_link(link_text, link_path)
+        class_name = current_page?(link_path) ? 'active' : ''
+
+        content_tag(:li, :class => class_name) do
+          link_to link_text, link_path
+        end
+      end
 end
