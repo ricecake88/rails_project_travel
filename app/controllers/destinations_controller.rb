4e919ab37d85d694136700bbf9103933deb6de84
ricecake88 <vocalise@gmail.com>
2019-11-16 16:29:55 -0800

    Added destination model/controller and views

---
+++ app/controllers/destinations_controller.rb
@@ -0,0 +1,55 @@
+class DestinationsController < ApplicationController
+    def index
+        @destinations = Destination.all
+    end
+
+    def new
+        @destination = Destination.new
+    end
+
+    def show
+        @destination = Destination.find_by(:id => params[:id])
+    end
+
+    def create
+        @destination = Destination.new(destination_params)
+        if @destination.save
+            flash[:notice] = "Destination created."
+            redirect_to destination_path(@destination)
+        else
+            flash[:notice] = "Destination Failed."
+            flash[:alert] = helpers.flash_error_message(@destination)
+            redirect_to destinations_path
+        end
+    end
+
+    def edit
+        @destination = Destination.find_by(:id => params[:id])
+    end
+
+    def update
+        @destination = Destination.find_by(:id => params[:id])
+        if @destination.update(destination_params)
+            redirect_to destination_path(@destination)
+        else
+            flash[:notice] = "Edit Destination Failed."
+            redirect_to destinations_path
+        end
+    end
+
+    def destroy
+        @destination = Destination.find_by(:id => params[:id])
+        @destination.destroy
+        redirect_to destinations_path
+    end
+
+    def my_destinations
+        @destinations = Destination.user_destinations(current_user)
+    end
+
+    private
+    def destination_params
+        params.require(:destination).permit(:city_name, :country_name)
+    end
+
+end
---
+++ app/models/destination.rb
@@ -0,0 +1,23 @@
+class Destination < ApplicationRecord
+    has_many :legs
+    has_many :vacations, through: :legs
+    has_many :attractions
+    validates :city_name, presence: { message: 'cannot be blank.' }
+    validates :country_name, presence: { message: 'cannot be blank.' }
+    validates_uniqueness_of :city_name, scope: [:state_name, :country_name], message: 'already entered.'
+    validates_format_of :city_name, :country_name, :with => /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/
+
+    def self.user_destinations(user)
+
+        user_destinations = []
+        user.vacations.each do |vacation|
+            vacation.destinations.uniq.each do |dest|
+                if !user_destinations.include?(dest)
+                    user_destinations << Destination.find_by(:id => dest.id)
+                end
+            end
+        end
+        return user_destinations
+    end
+
+end
