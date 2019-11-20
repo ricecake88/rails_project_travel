79ceafb83f13cce2f40d9da49fed7ac40a7b304e
ricecake88 <vocalise@gmail.com>
2019-11-19 22:43:57 -0800

    Updated seeds with Destination

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-@sandiego = Place.create(:city_name => "San Diego", :country_name => "United States")
-@seattle = Place.create(:city_name => "Seattle", :country_name => "United States")
-@seattle_1 = Attraction.create(:name => "Space Needle", :admission => "$10", :webpage => "Space Needle URL", :place_id => @seattle.id)
-@seattle_2 = Attraction.create(:name => "Chihuly Museum", :admission => "$24", :webpage => "Chihuly Museum URL", :place_id => @seattle.id)
+@sandiego = Destination.create(:city_name => "San Diego", :country_name => "United States")
+@seattle = Destination.create(:city_name => "Seattle", :country_name => "United States")
+@seattle_1 = Attraction.create(:name => "Space Needle", :admission => "$10", :webpage => "Space Needle URL", :destination_id => @seattle.id)
+@seattle_2 = Attraction.create(:name => "Chihuly Museum", :admission => "$24", :webpage => "Chihuly Museum URL", :destination_id => @seattle.id)
 
-@banff = Place.create(:city_name => "Banff", :country_name => "Canada")
-@banff_1 = Attraction.create(:name => "Lake Louise", :admission => "Free", :webpage => "Lake Louise URL", :place_id => @banff.id)
+@banff = Destination.create(:city_name => "Banff", :country_name => "Canada")
+@banff_1 = Attraction.create(:name => "Lake Louise", :admission => "Free", :webpage => "Lake Louise URL", :destination_id => @banff.id)
