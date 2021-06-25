9ebda34c399acb9cb1c00377465428ba1d2e5e4a
Grace S <vocalise@gmail.com>
2021-06-25 00:14:08 -0700

    Fixed change_column line

--- db/migrate/20190908043553_change_data_type_for_admission.rb
+++ db/migrate/20190908043553_change_data_type_for_admission.rb
@@ -1,5 +1,5 @@
 class ChangeDataTypeForAdmission < ActiveRecord::Migration[5.2]
   def change
-    change_column :attractions, :admission, 'integer USING CAST(column_name AS integer)'
+    change_column :attractions, :admission, 'integer USING CAST(admission AS integer)'
   end
 end
